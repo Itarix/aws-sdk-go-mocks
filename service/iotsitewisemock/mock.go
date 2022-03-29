ec06b11094f144e74e18e8283e522d35baa1d586
Repository CI <quentin.cousin76400@gmail.com>
2022-03-29 23:18:56 +0000

    Automatic ci update Version into v1.37.7

--- service/iotsitewisemock/mock.go
+++ service/iotsitewisemock/mock.go
@@ -535,56 +535,6 @@ func (mr *MockIoTSiteWiseAPIMockRecorder) CreatePortalWithContext(arg0, arg1 int
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreatePortalWithContext", reflect.TypeOf((*MockIoTSiteWiseAPI)(nil).CreatePortalWithContext), varargs...)
 }
 
-// CreatePresignedPortalUrl mocks base method
-func (m *MockIoTSiteWiseAPI) CreatePresignedPortalUrl(arg0 *iotsitewise.CreatePresignedPortalUrlInput) (*iotsitewise.CreatePresignedPortalUrlOutput, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "CreatePresignedPortalUrl", arg0)
-	ret0, _ := ret[0].(*iotsitewise.CreatePresignedPortalUrlOutput)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// CreatePresignedPortalUrl indicates an expected call of CreatePresignedPortalUrl
-func (mr *MockIoTSiteWiseAPIMockRecorder) CreatePresignedPortalUrl(arg0 interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreatePresignedPortalUrl", reflect.TypeOf((*MockIoTSiteWiseAPI)(nil).CreatePresignedPortalUrl), arg0)
-}
-
-// CreatePresignedPortalUrlRequest mocks base method
-func (m *MockIoTSiteWiseAPI) CreatePresignedPortalUrlRequest(arg0 *iotsitewise.CreatePresignedPortalUrlInput) (*request.Request, *iotsitewise.CreatePresignedPortalUrlOutput) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "CreatePresignedPortalUrlRequest", arg0)
-	ret0, _ := ret[0].(*request.Request)
-	ret1, _ := ret[1].(*iotsitewise.CreatePresignedPortalUrlOutput)
-	return ret0, ret1
-}
-
-// CreatePresignedPortalUrlRequest indicates an expected call of CreatePresignedPortalUrlRequest
-func (mr *MockIoTSiteWiseAPIMockRecorder) CreatePresignedPortalUrlRequest(arg0 interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreatePresignedPortalUrlRequest", reflect.TypeOf((*MockIoTSiteWiseAPI)(nil).CreatePresignedPortalUrlRequest), arg0)
-}
-
-// CreatePresignedPortalUrlWithContext mocks base method
-func (m *MockIoTSiteWiseAPI) CreatePresignedPortalUrlWithContext(arg0 context.Context, arg1 *iotsitewise.CreatePresignedPortalUrlInput, arg2 ...request.Option) (*iotsitewise.CreatePresignedPortalUrlOutput, error) {
-	m.ctrl.T.Helper()
-	varargs := []interface{}{arg0, arg1}
-	for _, a := range arg2 {
-		varargs = append(varargs, a)
-	}
-	ret := m.ctrl.Call(m, "CreatePresignedPortalUrlWithContext", varargs...)
-	ret0, _ := ret[0].(*iotsitewise.CreatePresignedPortalUrlOutput)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// CreatePresignedPortalUrlWithContext indicates an expected call of CreatePresignedPortalUrlWithContext
-func (mr *MockIoTSiteWiseAPIMockRecorder) CreatePresignedPortalUrlWithContext(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	varargs := append([]interface{}{arg0, arg1}, arg2...)
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreatePresignedPortalUrlWithContext", reflect.TypeOf((*MockIoTSiteWiseAPI)(nil).CreatePresignedPortalUrlWithContext), varargs...)
-}
-
 // CreateProject mocks base method
 func (m *MockIoTSiteWiseAPI) CreateProject(arg0 *iotsitewise.CreateProjectInput) (*iotsitewise.CreateProjectOutput, error) {
 	m.ctrl.T.Helper()
